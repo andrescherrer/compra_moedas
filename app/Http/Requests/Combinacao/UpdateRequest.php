b6da6a53b0d419601c001b19714ea29af1e2790a
André C. Scherrer <andrescherrer@gmail.com>
2025-04-14 03:33:57 -0300

    refactor: add messages function Combinacao/UpdateRequest

--- app/Http/Requests/Combinacao/UpdateRequest.php
+++ app/Http/Requests/Combinacao/UpdateRequest.php
@@ -46,6 +46,12 @@ class UpdateRequest extends FormRequest
             'codigo.unique' => 'O código já está em uso.',
             'codigo_moeda_base.unique' => 'O código da moeda base já está em uso.',
             'descricao.unique' => 'A descrição já está em uso.',
+            'codigo.max' => 'O código deve ter no máximo 9 caracteres.',
+            'codigo_moeda_base.max' => 'O código da moeda base deve ter no máximo 5 caracteres.',
+            'descricao.max' => 'A descrição deve ter no máximo 255 caracteres.',
+            'codigo.string' => 'O código deve ser uma string.',
+            'codigo_moeda_base.string' => 'O código da moeda base deve ser uma string.',
+            'descricao.string' => 'A descrição deve ser uma string.',
         ];
     }
 }
