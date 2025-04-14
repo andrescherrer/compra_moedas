2a0972517a1db350b317cead5e3fc142eddffd3d
André C. Scherrer <andrescherrer@gmail.com>
2025-04-14 01:33:54 -0300

    feat: implements store method CombinacaoController

--- app/Http/Controllers/CombinacaoController.php
+++ app/Http/Controllers/CombinacaoController.php
@@ -2,21 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Combinacao\IndexRequest;
+use App\Http\Requests\Combinacao\{IndexRequest, StoreRequest};
 use App\Http\Resources\Combinacao\IndexCollection;
 use App\Models\Combinacao;
 use App\Services\CombinacaoService;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
 
 class CombinacaoController extends Controller
 {
     private $arrayErrorMessage = [
-        'index' => 'Erro ao listar combinações',
-        'store' => 'Erro ao criar combinação',
-        'show' => 'Erro ao exibir combinação',
-        'update' => 'Erro ao atualizar combinação',
-        'destroy' => 'Erro ao deletar combinação',
+        'index' => 'Erro ao listar combinações ',
+        'store' => 'Erro ao criar combinação ',
+        'show' => 'Erro ao exibir combinação ',
+        'update' => 'Erro ao atualizar combinação ',
+        'destroy' => 'Erro ao deletar combinação ',
     ];
 
     public function __construct(
@@ -34,18 +35,31 @@ class CombinacaoController extends Controller
             return new IndexCollection($this->service->filter($request));
 
         } catch (\Throwable $th) {
-            return response()->json([
-                'message' => $message,
-                'error' => $th->getMessage(),
-            ], $status);
+            Log::critical($message .  $th->getMessage());
+            return response()->json(['message' => $message, 'error' => $th->getMessage()], $status);
         }
     }
 
-    public function store(Request $request)
+    public function store(StoreRequest $request): JsonResponse
     {
-        //
-    }
+        $message = $this->arrayErrorMessage['store'];
+        $status = JsonResponse::HTTP_BAD_REQUEST;
+        
+        try {
+            $combinacao = $this->service->create($request);
 
+            if (!$combinacao) {
+                return response()->json(['message' => $message,], $status);
+            } else {
+                return response()->json(['message' => 'Combinacao criada com sucesso'], JsonResponse::HTTP_CREATED);
+            }
+            
+        } catch (\Throwable $th) {
+            Log::critical($message .  $th->getMessage());
+            return response()->json(['message' => $message, 'error' => $th->getMessage()], $status);
+        }
+    }
+    
     public function show(Combinacao $combinacao)
     {
         //
